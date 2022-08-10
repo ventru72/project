40c1a0a9f5838efafbd5b980ab1af2c2c91be20e
Tigi <ventru72@yandex.ru>
2022-08-10 18:23:15 +0500

    commit 3 некотоыре успехи в графическом интерфейсе

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
+using Dapper;
+using System.Data;
+using System.Collections.ObjectModel;
 
 namespace project
 {
@@ -20,10 +24,87 @@ namespace project
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
+            
+            string selectQuery = $@"select 
+        id_project,
+        name_project
+        from project";
             InitializeComponent();
-             
+          
+
+            Sql_Requests sql_Requests = new Sql_Requests();
+           
+            List<Project> date_projects = sql_Requests.Select(selectQuery);
+            List<string> date_projects2 = new List<string>();
+           // comboBox_projects.DataContext = date_projects;
+            foreach (Project project in date_projects)
+            {
+                date_projects2.Add(project.name_project.ToString());
+            }
+
+
+            dataGrid.ItemsSource = date_projects;
+            comboBox1.ItemsSource = date_projects2;
+
+
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Sql_Requests sql_Requests = new Sql_Requests();
+            string selectQuery = $@"select 
+        id_project,
+        name_project
+        from project";
+
+           
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+            string selectQuery = $@"select 
+        id_project,
+        name_project
+        from project";
+            Sql_Requests sql_Requests = new Sql_Requests();
+            List<Project> date_projects = sql_Requests.Select(selectQuery);
+
+            List<string> list_combo = new List<string>();
+
+            foreach (Project project in date_projects)
+            {
+                list_combo.Add(project.name_project.ToString());
+            }
+            
+        }
+        private void Combo_Box_loaded(object sender, SelectionChangedEventArgs e)
+        {
+           
+             var combo = sender as ComboBox;
+           
+            List<string> list_combo = new List<string>();
+            list_combo.Add("dfdff");
+            list_combo.Add("fdfdf");
+            combo.ItemsSource = list_combo;
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            
+            Sql_Requests sql_Requests = new Sql_Requests();
+            comboBox1.DataContext = comboBox1.SelectedItem;
+            string selectQuery = $"SELECT id_design_object, name_object, code" +
+                $" FROM project " +
+                $"LEFT JOIN design_object ON project.id_executor = design_object.id_executor " +
+                $"WHERE name_project = '{comboBox1.DataContext.ToString()}'";
+            List<Project> date_projects = sql_Requests.Select(selectQuery);
+            listBox.ItemsSource = date_projects;
+
         }
     }
-}
+} 
--- Object_List_Box.cs
+++ Object_List_Box.cs
@@ -8,88 +8,94 @@ namespace project
 {
     public class Dictionary
     {
-        public int Id_Stamps { get; set; }
-        public string Stamps_Short_Name { get; set; }
-        public string Stamps_Full_Name { get; set; }
+        public int id_stamps { get; set; }
+        public string stamps_Sshort_name { get; set; }
+        public string stamps_full_name { get; set; }
 
-        public int Id_Type_Documents { get; set; }
-        public string Type_Documents_Short_Name { get; set; }
-        public string Type_Documents_Full_Name { get; set; }
+        public int id_type_documents { get; set; }
+        public string type_documents_short_name { get; set; }
+        public string type_documents_full_Name { get; set; }
 
-        public int Id_Executor { get; set; }
-        public string Executor_Short_Name { get; set; }
-        public string Executor_Full_Name { get; set; }
-        public Dictionary(string Stamps_Short_Name)
-        {
-            this.Stamps_Short_Name = Stamps_Short_Name;
-        }
+        public int id_executor { get; set; }
+        public string executor_short_name { get; set; }
+        public string executor_full_name { get; set; }
+        //public Dictionary(string Stamps_Short_Name)
+        //{
+        //    this.Stamps_Short_Name = Stamps_Short_Name;
+        //}
         public Dictionary() { }
 
     }
+    public class Project_name
+    {
+        public List<string> name_project { get; set; }
+    }
     public class Project: Dictionary
     {
-        public int Id_Project { get; set; }
-       
-        public string Name_Project { get; set; }
-        public string Executor { get; set; }
-        public DateTime Dadata_Creation { get; set; }
+        public int id_project { get; set; }
+        
+        public string cipher { get; set; }
+        public string name_project { get; set; }
+        public int  id_executors { get; set; }
+        public DateTime dadata_Creation { get; set; }
         public DateTime Dadata_Change { get; set; }
-        public Project(string Stamps_Short_Name, DateTime Dadata_Creation, DateTime Dadata_Change):base(Stamps_Short_Name)
-        {
+        //public Project(string Stamps_Short_Name, DateTime Dadata_Creation, DateTime Dadata_Change):base(Stamps_Short_Name)
+        //{
 
-        }
+        //}
         public Project() { }
 
     }
     public class Design_Object : Project
     {
-        public int Id_Design_Object { get; set; }
-        public int Id_Parent { get; set; }
-        public string Name_Object { get; set; }
-        public string Code { get; set; }
-        public string Full_Code { get; set; }
-        public int Id_Set_Documentation { get; set; }
-        public string Сomposite_Stamps_Do { get; set; }
-        public Design_Object(int Id_Design_Object, string Full_Code, string Stamps_Short_Name, string Сomposite_Stamps_Do, string Executor, DateTime Dadata_Creation, DateTime Dadata_Change) : base(Stamps_Short_Name, Dadata_Creation, Dadata_Change)
-        {
-            this.Id_Design_Object = Id_Design_Object;
-            this.Full_Code = Full_Code;
-            this.Stamps_Short_Name = Stamps_Short_Name;
-            this.Сomposite_Stamps_Do = Сomposite_Stamps_Do;
-            this.Executor = Executor;
-            this.Dadata_Creation = Dadata_Creation;
-            this.Dadata_Change = Dadata_Change;
-        }
+        public int id_design_object { get; set; }
+        public int id_parent { get; set; }
+        public string name_object { get; set; }
+        public string code { get; set; }
+        public string full_code { get; set; }
+        public int id_set_documentation { get; set; }
+        public string composite_stamps_do { get; set; }
+        //public Design_Object(int Id_Design_Object, string Full_Code, string Stamps_Short_Name, string Сomposite_Stamps_Do, string Executor, DateTime Dadata_Creation, DateTime Dadata_Change) : base(Stamps_Short_Name, Dadata_Creation, Dadata_Change)
+        //{
+        //    this.Id_Design_Object = Id_Design_Object;
+        //    this.Full_Code = Full_Code;
+        //    this.Stamps_Short_Name = Stamps_Short_Name;
+        //    this.Сomposite_Stamps_Do = Сomposite_Stamps_Do;
+        //    this.executor = Executor;
+        //    this.Dadata_Creation = Dadata_Creation;
+        //    this.Dadata_Change = Dadata_Change;
+        //}
         public Design_Object(){}
     }
     public class Set_Documentation : Design_Object
     {
     
 
-        public int Number_Set_Documentation { get; set; }
-        public string Name_Set_Documentation { get; set; }
-        public string Сomposite_Stamps_Sd { get; set; }
-        public int Id_Documents { get; set; }
+        public int number_set_documentation { get; set; }
+        public string name_set_documentation { get; set; }
+        public string composite_stamps_sd { get; set; }
+        public int id_documents { get; set; }
         public Set_Documentation() { }
        
 
     }
     public class Documents : Set_Documentation
     {
-        public int Number_Documents { get; set; }
-        public string Name_Documents { get; set; }
-        public string Сomposite_Stamps_Doc { get; set; }
+        public int number_documents { get; set; }
+        public string name_documents { get; set; }
+        public string composite_stamps_doc { get; set; }
+
        
-        public Documents(int Id_Documents, string Stamps_Short_Name, int Number_Documents, string Name_Documents, string Сomposite_Stamps_Doc,  DateTime Dadata_Creation, DateTime Dadata_Change)  
-        {
-            this.Id_Documents = Id_Documents;
-            this.Number_Documents = Number_Documents;
-            this.Stamps_Short_Name = Stamps_Short_Name;
-            this.Сomposite_Stamps_Doc = Сomposite_Stamps_Doc;
-            this.Name_Documents = Name_Documents;
-            this.Dadata_Creation = Dadata_Creation;
-            this.Dadata_Change = Dadata_Change;
-        }
+        //public Documents(int Id_Documents, string Stamps_Short_Name, int Number_Documents, string Name_Documents, string Сomposite_Stamps_Doc,  DateTime Dadata_Creation, DateTime Dadata_Change)  
+        //{
+        //    this.Id_Documents = Id_Documents;
+        //    this.Number_Documents = Number_Documents;
+        //    this.Stamps_Short_Name = Stamps_Short_Name;
+        //    this.Сomposite_Stamps_Doc = Сomposite_Stamps_Doc;
+        //    this.Name_Documents = Name_Documents;
+        //    this.Dadata_Creation = Dadata_Creation;
+        //    this.Dadata_Change = Dadata_Change;
+        //}
     }
   
 
